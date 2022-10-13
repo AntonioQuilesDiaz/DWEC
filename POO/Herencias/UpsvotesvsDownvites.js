8b65f88fa2f457b4f3588933d99d0f8c8d337274
AntonioQuilesDiaz <95092656+AntonioQuilesDiaz@users.noreply.github.com>
2022-10-13 20:35:36 +0200

    a

---
+++ POO/Herencias/UpsvotesvsDownvites.js
@@ -0,0 +1,7 @@
+function UpvotesvsDownvotes(){
+    let upvotes = 10;
+    let downvotes = -12;
+    let resultado = upvotes + downvotes;
+    console.log(resultado);
+}
+console.log(UpvotesvsDownvotes());
