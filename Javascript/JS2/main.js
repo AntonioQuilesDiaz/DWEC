23605cde9c73ea702959addf3441242a79365262
AntonioQuilesDiaz <antonio.quiles@adaits.es>
2022-11-16 23:20:01 +0100

    a

---
+++ Javascript/JS2/main.js
@@ -0,0 +1,124 @@
+var paises = [
+    {
+        "pais" : "España",
+        "capital" : "Madrid"
+    },
+    {
+        "pais" : "Alemania",
+        "capital" : "Berlín"
+    },
+    {
+        "pais" : "Albania",
+        "capital" : "Tirana"
+    },
+    {
+        "pais" : "Andorra",
+        "capital" : "Adorra la Vella"
+    },
+    {
+        "pais" : "Francia",
+        "capital" : "Paris"
+    },
+    {
+        "pais" : "Reino Unido",
+        "capital" : "Londres"
+    },
+    {
+        "pais" : "Bélgica",
+        "capital" : "Bruselas"
+    },
+    {
+        "pais" : "Croacia",
+        "capital" : "Zagreb"
+    },
+    {
+        "pais" : "Dinamarca",
+        "capital" : "Copenhage"
+    },
+    {
+        "pais" : "Eslovaquia",
+        "capital" : "Bratislava"
+    },
+    {
+        "pais" : "Estonia",
+        "capital" : "Tallin"
+    },
+    {
+        "pais" : "Finlandia",
+        "capital" : "Helsinki"
+    },
+    {
+        "pais" : "Italia",
+        "capital" : "Roma"
+    },
+    {
+        "pais" : "Luxemburgo",
+        "capital" : "Luxemburgo"
+    },
+    {
+        "pais" : "Monáco",
+        "capital" : "Moncáco"
+    },
+    {
+        "pais" : "Países Bajos",
+        "capital" : "Ámsterdam"
+    },
+    {
+        "pais" : "Polonia",
+        "capital" : "Varsovia"
+    },
+    {
+        "pais" : "Portugal",
+        "capital" : "Lisboa"
+    }
+]
+
+let tablas = document.getElementById("tabla");
+
+let generarTabla = '';
+
+for (x of paises) {
+    generarTabla += `<tr><td>${x.capital}</td><td>${x.pais}</td</tr>`
+}
+
+tablas.innerHTML += generarTabla;
+
+function Capital() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value;
+    table = document.getElementById("tabla");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+
+
+function Pais() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput2");
+    filter = input.value;
+    table = document.getElementById("tabla");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
