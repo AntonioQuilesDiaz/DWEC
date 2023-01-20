300c9554151778dd1a2fd0f87935b20018bb448a
AntonioQuilesDiaz <95092656+AntonioQuilesDiaz@users.noreply.github.com>
2023-01-20 17:27:42 +0100

    Actividad avanzada completada

--- Angular/A1/angularUno/src/app/app.module.ts
+++ Angular/A1/angularUno/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioSecionComponent } from './inicio-secion/inicio-secion.component';
-import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
+import { InicioSecionComponent } from './Componentes/inicio-secion/inicio-secion.component';
+import { CrearCuentaComponent } from './Componentes/crear-cuenta/crear-cuenta.component';
 
 @NgModule({
   declarations: [
